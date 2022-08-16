f38f011fada8dd84429a713621923e7e91d4a4ea
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2022-08-15 21:35:55 -0300

    refactor: adds index files with multiple imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Login from '@/presentation/pages/login/login'
+import { Login } from '@/presentation/pages'
 
 const container = document.getElementById('main')
 const root = createRoot(container)
