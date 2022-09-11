68b1bccc727909f7223eb924fdcc7fdf4b657ab4
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2022-09-11 20:31:40 -0300

    fix: moves global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import { Router } from '@/presentation/components'
 
+import '@/presentation/styles/global.scss'
+
 const container = document.getElementById('main')
 const root = createRoot(container)
 
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Login } from '@/presentation/pages'
 
-import '@/presentation/styles/global.scss'
-
 const Router: React.FC = () => {
   return <BrowserRouter>
     <Routes>
