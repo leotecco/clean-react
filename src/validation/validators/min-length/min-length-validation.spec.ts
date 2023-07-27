71d8857dd0a47f9d0115cc82e5cc9a115dffdfa6
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2023-07-26 23:11:45 -0300

    feat: creates MinLengthValidation

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+import { MinLengthValidation } from './min-length-validation'
+import { InvalidFieldError } from '@/validation/errors'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(4))
+    expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return error if value is invalid', () => {
+    const sut = makeSut()
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeFalsy()
+  })
+})
