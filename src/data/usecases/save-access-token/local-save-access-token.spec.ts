534d728ab777a7dc488aa5fd02e2bddd7ac22a87
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2023-08-09 22:33:06 -0300

    feat: creates SaveAccessToken use case

---
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -0,0 +1,30 @@
+import { LocalSaveAccessToken } from './local-save-access-token'
+import { SetStorageSpy } from '@/data/test/mock-storage'
+import { faker } from '@faker-js/faker'
+
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+
+  return {
+    sut,
+    setStorageSpy
+  }
+}
+
+describe('LocalSaveAccessToken', () => {
+  test('Should call SetStorage with correct value', async () => {
+    const { sut, setStorageSpy } = makeSut()
+    const accessToken = faker.string.uuid()
+
+    await sut.save(accessToken)
+
+    expect(setStorageSpy.key).toBe('accessToken')
+    expect(setStorageSpy.value).toBe(accessToken)
+  })
+})
