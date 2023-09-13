6cd446613a3656e5a3aa12bd871754084c3b6848
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2023-09-12 21:40:24 -0300

    feat: adds signup view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
+import { SignUp } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return <BrowserRouter>
     <Routes>
       <Route path='/'>
         <Route path="login" element={makeLogin()} />
+        <Route path="signup" element={<SignUp />} />
       </Route>
     </Routes>
   </BrowserRouter>
