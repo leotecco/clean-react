1e59beb711f044469df74a9095b16831e27ece23
Leonardo Jorge Tecco <teccoleo@hotmail.com>
2025-01-15 12:22:30 -0300

    feat: apply PrivateRoute to SurveyList page

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
+import { PrivateRoute } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
 import { SurveyList } from '@/presentation/pages'
 import { getCurrenctAccountAdapter, setCurrenctAccountAdapter } from '../adapters/current-account-adapter'
@@ -13,11 +14,13 @@ const Router: React.FC = () => {
     <ApiContext.Provider value={{ setCurrentAccount: setCurrenctAccountAdapter, getCurrentAccount: getCurrenctAccountAdapter }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/">
-            <Route path="login" element={makeLogin()} />
-            <Route path="signup" element={makeSignUp()} />
-            <Route path="/" element={<SurveyList />} />
+          <Route path="/" element={<PrivateRoute />}>
+            <Route element={<SurveyList />} index />
           </Route>
+
+          <Route path="login" element={makeLogin()} />
+          <Route path="signup" element={makeSignUp()} />
+          <Route path="*" element={<h1>Page not found</h1>} />
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
